8f887b1c6ca412b5f2c3badec4386ba8a48fa42c
Michelle <232309167+MichelleRunning@users.noreply.github.com>
2026-01-20 07:51:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ window.title("Password Manager")
 window.config(padx=30, pady=30)
 
 canvas = Canvas(width=200, height=200)
-logo_img = PhotoImage(file="logo.png")
+logo_img = PhotoImage(file="logo1.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
